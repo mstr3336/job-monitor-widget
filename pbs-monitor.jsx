e2d2f5420ed51395acd22f9bd27c0bd5004c70e9
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-01-20 22:30:26 +1100

    Update main widget - States are beginning to fetch successfully

--- pbs-monitor.jsx
+++ pbs-monitor.jsx
@@ -28,28 +28,64 @@ const cmds = {
 	'job_info'  : 'get_job_info'
 }
 
-export const refreshFrequency = 60000;
+
+export const refreshFrequency = 1000*30;
 
 
 export const command = (dispatch) => {
-	let joblist = run('ssh ' + info.username + '@' + info.server + ' ' + cmds.query_all);
-	
-	dispatch({ type: 'JOBLIST_FETCHED', data : joblist});
-	
-		
+	run('ssh ' + info.username + '@' + info.server + ' ' + cmds.query_all)
+		.then((output) => dispatch({ type: 'JOBLIST_FETCHED', output}));
 
 }
 
-export const inititalState = { output : 'fetching jobs' };
+export const inititalState = { 
+	jobList : [], 
+	jobStatuses : {}
+};
+
+function updateJoblist(result, previousState) {
+	let data = JSON.parse(result);
+
+	previousState['jobList'] = data;
+
+	for (let job of data ) {
+		console.info("Fetching data for " + job);
+		var job_status = run('ssh ' + info.username + '@' + info.server + ' ' + cmds.job_info + ' ' + job);
+
+		
+		job_status
+			.then((status) => {console.warn(status)});
+			//.then((status) => updateState({type : 'JOB_STATUS_FETCHED', output : { job_id : job, status }}), previousState);
+
+		console.info(job_status);
+
+	}
+
+	return previousState;
+}
+
+function updateJobStatus(result, previousState) {
+	result.status = JSON.parse(result.status);
+	console.warn(result);
+
+	previousState.jobStatuses[result.job_id] = result.status;
+	return previousState;
+}
 
 export const updateState = (event, previousState) => {
-		print(event);
+		console.warn(event);
+		switch(event.type) {
+			case 'JOBLIST_FETCHED' : return updateJoblist(event.output, previousState);
+			case 'JOB_STATUS_FETCHED' : return updateJobStatus(event.output, previousState);
+			default : return previousState;
+		}
 }
 
-export const render = ({ state }) => {
+export const render = ( state ) => {
+	console.debug( state );
 	return(
 		<div>
-			<h1> Hello </h1>
+			<h1> {state.jobList} </h1>
 
 		</div>
 		)
