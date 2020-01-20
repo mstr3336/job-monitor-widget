32ec9fe0f81cde8992f0f794f283553a79d402ae
Matthew Mark Strasiotto <mstr3336@uni.sydney.edu.au>
2020-01-20 18:30:40 +1100

    Improving way commands take args

--- cmds.sh
+++ cmds.sh
@@ -19,11 +19,34 @@ function get_job_ids() {
 function get_job_info() {
 	local job_id=$1
 	local raw_output="$(qstat -f $job_id)"
-
+         
+	#echo "$raw_output"
 	local useful_keys="Resource_List|resources_used|Job_Name|job_state|array"
 
-	filtered_output=echo $raw_output |& \
-	  grep -ie $useful_keys 
+	local filtered_output=$(echo "$raw_output" |& grep -iE "$useful_keys")
+	echo "$filtered_output"
 
-	echo $filtered_output
+	local resource_list=$(echo "$raw_output" |& sed -eE '/Resource_List/p')
+}
+
+function make_json_dict() {
+        local input=$1; if [ "$input" == "-" ]; then read input; fi
+        
+	echo "$input" |& \
+	  sed -Ee 's/^(.+)\s+=\s*(.+)/"\1":"\2",/g;
+	           1s;^;{\n;
+		   $s/,$/\n}\n/'
+
+}
+
+
+function get_sublist() {
+        local string=$1; if [ "$string" == "-" ]; then read string; fi
+        local prefix="$2"
+	
+	echo "$string" |& \
+	  sed -nEe '/$prefix/p' |& \
+	  sed -Ee  's/^.*\.//'  |& \
+	  make_json_dict
+
 }
