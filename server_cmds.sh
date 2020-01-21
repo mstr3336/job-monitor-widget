51b9683d071d89ecc2c0e114bbe89ffbd805ef13
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-01-21 20:07:43 +1100

    change delimiter mechanism

--- server_cmds.sh
+++ server_cmds.sh
@@ -111,12 +111,12 @@ function separate_joblists_2() {
 
 		while read -r -d $'\0' each; do   # use a NUL terminated field separator 
 		    array+=("$each")
-		done < <(printf "%s" "$str" | awk '{ gsub(/Job Id:/,"\0"job_id = ); print }')
+		done < <(printf "%s" "$str" | awk '{ gsub(/Job Id:/,"\0"); print }')
 		
 		#declare -p array 
 
 		for job in "${array[@]}"; do
-			echo "$job" |& \
+			echo "job_id=$job" | \
 			  get_job_info
 		done
 }
