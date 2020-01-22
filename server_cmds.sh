a48346b95b3725ae0ed32230eb5bc985bd26a4e6
Matthew Mark Strasiotto <mstr3336@uni.sydney.edu.au>
2020-01-22 17:09:08 +1100

    Removed defunct bash functions

--- server_cmds.sh
+++ server_cmds.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# module load python/3.7.2
+
 # Intended to run serverside
 function get_job_ids() {
     # Remove Everything before a line of only --- and whitespace
@@ -11,75 +13,10 @@ function get_job_ids() {
 		sed -Ee '1,/[-\s]+$/ d;s/ .*//' 
 	 	 
 }
-function get_job_ids_json() {
-	get_job_ids |& \
-	       	sed -Ee 's/(.*)/"\1",/g;
-		         1s;^;[\n;
-		         $s/,$/\n]\n/'
-	
-}
 
 function get_all_jobs_raw() {
 	local job_ids=$(get_job_ids)
 
 	qstat -ft ${job_ids}
-}
-
-function get_job_info() {
-	local job_id=$1
-	local raw_output="$(qstat -f $job_id)"
-         
-	local useful_keys="(Job_Name|job_state|array|job_id)"
-
-	local filtered_output=$(echo "$raw_output" |& grep -iE "^\s*$useful_keys")
-	
-	local general_info=$(echo "$filtered_output" |& make_json_dict -)
-
-	local resource_list=$(echo "$raw_output" |& get_sublist - 'Resource_List')
-	local resource_used=$(echo "$raw_output" |& get_sublist - 'resources_used')
-
-	echo $(cat <<-EOF
-	{
-		"info"          : $general_info,
-		"resource_list" : $resource_list,
-		"resource_used" : $resource_used
-	}
-	EOF
-	);
-}
-
-function make_json_dict() {
-        local line=""
-        local input=$1; if [ "$input" == "-" ]; then 
-		input=""
-		while read line; do
-			input="${input}\n${line}"	
-		done	
-        fi
-
-	echo -e "$input" |& \
-	  sed -Ee 's/^(.+)\s+=\s*(.+)/"\1":"\2",/g;
-	           1s;^;{\n;
-		   $s/(,\s*)?$/\n}\n/'
-
-}
-
-
-function get_sublist() {
-        local line=""
-        local input=$1; if [ "$input" == "-" ]; then 
-		input=""
-		while read line; do
-			input="${input}\n${line}"	
-		done	
-        fi
-        
-        local prefix="$2"
-
-	echo -e "$input" |& \
-	  sed -nEe "/${prefix}/p" |& \
-	  sed -Ee  's/^.*\.//'  |& \
-	  make_json_dict -
-
 }
 
