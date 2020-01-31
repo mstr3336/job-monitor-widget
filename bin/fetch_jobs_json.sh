dc32765631ec88c50bcc3787e4240252eb7d12ad
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-01-31 16:55:41 +1100

    Update fetch_jobs_json.sh

--- bin/fetch_jobs_json.sh
+++ bin/fetch_jobs_json.sh
@@ -5,9 +5,10 @@ cd $scriptDir
 
 username="$USER"
 
-if [ ! -z "$1" ] && username="$1"
-
+if [ ! -z "$1" ]; then
+	username="$1"
+fi
 
 module load python/3.7.2
 
-python fetch_jobs.py $username
+python fetch_jobs.py "$username"
