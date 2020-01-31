c20e02308f0c11d1b7022ecf221c070962048441
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-01-31 16:51:46 +1100

    fetch_jobs_json now takes optional "user" arg

--- bin/fetch_jobs_json.sh
+++ bin/fetch_jobs_json.sh
@@ -3,6 +3,11 @@
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cd $scriptDir 
 
+username="$USER"
+
+if [ ! -z "$1" ] && username="$1"
+
+
 module load python/3.7.2
 
-python fetch_jobs.py
+python fetch_jobs.py $username
